ef524a11f03d242ac32456ef325260e16132dc38
zylnts <zylnts@yeah.net>
2024-05-10 18:09:56 +0800

    Add files via upload

---
+++ dayinmoxing.py
@@ -0,0 +1,13 @@
+#使用函数，每个定义函数负责一项任务
+def print_models(unprinted_designs,completed_models):
+     #模拟打印每个设计，直到没有未打印的设计为止，打印每个设计后，都将其移到列表completed_models中
+     while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        #模拟根据设计制作3S打印模型的过程
+        print("Printing models: " + current_design)
+        completed_models.append(current_design)
+def show_completed_models(completed_models):
+       #显示打印好的所有模型
+       print("\nThe following models hanve been printed:")
+       for completed_model in completed_models:
+              print(completed_model)
